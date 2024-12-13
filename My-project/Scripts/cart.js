ba01e6ca5768ccfa4a2f00636520ac4243985123
Ashutosh0411 <98958839+Ashutosh0411@users.noreply.github.com>
2024-12-13 13:07:39 +0530

    Responsive+removed redundant files

---
+++ My-project/Scripts/cart.js
@@ -0,0 +1,148 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  // cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (ShoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item, size } = x;
+        let sz;
+        if(size==1)
+        {
+          sz="Small"
+        }
+        else if(size==2)
+        {
+          sz="Medium"
+        }
+        else if(size==3)
+        {
+          sz="Large"
+        }
+        else{
+          sz=""
+        }
+        let search = data.find((y) => y.id === id) || [];
+        return `
+      <div class="cart-item">
+        <img width="150" src=../menu/${search.img} alt="" />
+        <div class="details">
+
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">₹ ${search.price+size*50}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi-x-circle-fill"></i>
+          </div>
+          ${sz}
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus"></i>
+          </div>
+
+          <h3>₹ ${item * (search.price+size*50)}</h3>
+        </div>
+      </div>
+      `;
+      })
+      .join(""));
+  } else {
+    ShoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="Menu.html">
+      <button class="HomeBtn">Back to Menu</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  TotalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+  // console.log(selectedItem.id);
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  TotalAmount();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id ,size} = x;
+        let search = data.find((y) => y.id === id) || [];
+
+        return item * (search.price+50*size);
+      })
+      .reduce((x, y) => x + y, 0);
+    // console.log(amount);
+    label.innerHTML = `
+    <h2>Total Bill : ₹ ${amount}</h2>
+    <a href="Checkout.html"><button class="checkout" >Checkout</button></a>
+    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+    
+    `;
+  } else return;
+};
+
+TotalAmount();
