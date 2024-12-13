680e2eac8e2c51e6471c84e6c276cce6f2fc768b
Ashutosh0411 <98958839+Ashutosh0411@users.noreply.github.com>
2024-12-13 13:18:06 +0530

    Updated file structure

--- pizza/My-project/Scripts/temp_script.js
+++
@@ -1,7 +0,0 @@
-let cart = document.querySelectorAll('.btns');
-
-for(i = 0; i < cart.length; i++) {
-    cart[i].addEventListener('click', () =>{
-        alert("Button clicked")
-    })
-}
--- pizza/My-project/Scripts/text.js
+++
@@ -1,8 +0,0 @@
-function SaveItem() {
-			
-	var name = document.forms.ShoppingList.name.value;
-	var data = document.forms.ShoppingList.data.value;
-	localStorage.setItem(name, data);
-	doShowAll();
-	
-}
